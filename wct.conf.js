0dbe7d5532285f997bd4d0ea5fe782422170e820
Sirisha Aluri <sirisha.aluri@ge.com>
2016-03-17 11:34:44 -0700

    Added travis.yml and all browsers to test in wct.conf.js

--- wct.conf.js
+++ wct.conf.js
@@ -1,11 +1,74 @@
-module.exports = {
+tmodule.exports = {
     verbose: true,
     plugins: {
         local: {
             browsers: ['chrome', 'firefox']
         },
         sauce: {
-          disabled: true
+          disabled: false,
+          "browsers": [
+             {
+          "browserName": "internet explorer",
+          "platform": "Windows 7",
+          "version": "10"
+        },
+        {
+          "browserName": "chrome",
+          "platform": "Windows 10",
+          "version": "44"
+        },
+         {
+          "browserName": "microsoftedge",
+          "platform": "Windows 10"
+         
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 8.1",
+          "version": "11"
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 10",
+          "version": "11"
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 8",
+          "version": "10"
+        },
+         {
+          "browserName": "firefox",
+          "platform": "Windows 8.1",
+          "version": "44"
+        },
+         {
+          "browserName": "chrome",
+          "platform": "Windows 8.1",
+          "version": "48"
+        },
+         {
+          "browserName": "safari",
+          "platform": "OS X 10.10",
+          "version": "8"
+        },
+         
+        
+         {
+          "browserName": "chrome",
+          "platform": "OS X 10.11",
+          "version": "48"
+        },
+             
+           
+        {
+          "browserName": "safari",
+          "platform": "OS X 10.11",
+          "version": "9"
+        }
+        
+        
+      ]
         }
     },
     suites: [
